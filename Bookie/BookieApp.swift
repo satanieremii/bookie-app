b5df1971aeb4796f42111c42b2040ab380e91df5
satanieremii <y.eremiy@icloud.com>
2026-05-10 13:51:21 +0200

    Initial Commit

---
+++ Bookie/BookieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookieApp.swift
+//  Bookie
+//
+//  Created by Yuliia Ieremii on 10/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct BookieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookieTests/BookieTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookieTests.swift
+//  BookieTests
+//
+//  Created by Yuliia Ieremii on 10/05/2026.
+//
+
+import Testing
+@testable import Bookie
+
+struct BookieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
